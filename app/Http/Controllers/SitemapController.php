03558de5997004f9d7895fda39131cd11da82699
Developer <developer@info-guide.ru>
2020-04-18 21:24:06 +0300

    Fixed styles

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
 use Illuminate\Http\Request;
 use Spatie\Sitemap\SitemapGenerator;
 
 class SitemapController extends Controller
 {
     public function sitemap() {
-        SitemapGenerator::create('https://info-guide.ru')->writeToFile('/storage/s.xml');
+        $pages = City::get();
+        dd($pages);
     }
 }
