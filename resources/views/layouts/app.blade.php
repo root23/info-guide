32d0d4636288670c20ca023fa996a9adde0658a7
Developer <developer@info-guide.ru>
2020-04-22 21:00:57 +0300

    Ya verifycation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="{{ URL::asset('/img/favicon.png') }}" type="image/x-icon"/>
+    <meta name="yandex-verification" content="1c39173a03f7c309" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
