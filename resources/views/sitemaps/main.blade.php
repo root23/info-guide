0fa0bfe3425a3f2eee27d639427a65f8577691a2
Developer <developer@info-guide.ru>
2020-10-31 10:38:09 +0300

    upd lastmod of index page

--- resources/views/sitemaps/main.blade.php
+++ resources/views/sitemaps/main.blade.php
@@ -1,7 +1,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
     <url>
         <loc>https://info-guide.ru/</loc>
-        <lastmod>2020-04-19</lastmod>
+        <lastmod>2020-10-31</lastmod>
     </url>
     <url>
         <loc>https://info-guide.ru/taxi/contacts/</loc>
