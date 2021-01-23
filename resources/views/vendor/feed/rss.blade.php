c47e0fff0a46e02e35fce81413526fd1cbdca742
root23 <hipstor.pnz@gmail.com>
2021-01-23 17:45:34 +0300

    upd yandex turbo page template

--- resources/views/vendor/feed/rss.blade.php
+++ resources/views/vendor/feed/rss.blade.php
@@ -2,26 +2,41 @@
 /* Using an echo tag here so the `<? ... ?>` won't get parsed as short tags */
 '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL
 ?>
-<rss version="2.0">
+<rss xmlns:yandex="http://news.yandex.ru"
+     xmlns:media="http://search.yahoo.com/mrss"
+     xmlns:turbo="http://turbo.yandex.ru"
+    version="2.0">
     <channel>
+        <!-- Информация о сайте-источнике -->
         <title><![CDATA[{{ $meta['title'] }}]]></title>
         <link><![CDATA[{{ url($meta['link']) }}]]></link>
         <description><![CDATA[{{ $meta['description'] }}]]></description>
         <language>{{ $meta['language'] }}</language>
-        <pubDate>{{ $meta['updated'] }}</pubDate>
+        <turbo:analytics></turbo:analytics>
+        <turbo:adNetwork></turbo:adNetwork>
 
         @foreach($items as $item)
-            <item>
-                <title><![CDATA[{{ $item->title }}]]></title>
+            <item turbo="true">
+                <!-- Информация о странице -->
+                <turbo:extendedHtml>true</turbo:extendedHtml>
                 <link>{{ url($item->link) }}</link>
-                <description><![CDATA[{!! $item->summary !!}]]></description>
-                <author><![CDATA[{{ $item->author }}]]></author>
-                <guid>{{ url($item->id) }}</guid>
+                <turbo:source></turbo:source>
+                <turbo:topic></turbo:topic>
                 <pubDate>{{ $item->updated->toRfc3339String() }}</pubDate>
-                @foreach($item->category as $category)
-                    <category>{{ $category }}</category>
-                @endforeach
+                <author><![CDATA[{{ $item->author }}]]></author>
+                <metrics>
+                    <yandex schema_identifier="Идентификатор">
+                        <breadcrumblist>
+                            <breadcrumb url="https://info-guide.ru" text="Домашняя"/>
+                            <breadcrumb url="https://info-guide.ru/cities" text="Города"/>
+                            <breadcrumb url="{{ url($item->link) }}" text="{{ $item->title }}"/>
+                        </breadcrumblist>
+                    </yandex>
+                </metrics>
+                <yandex:related></yandex:related>
+                <turbo:content>
+                    <![CDATA[{!! $item->summary !!}]]>
+                </turbo:content>
             </item>
-        @endforeach
     </channel>
 </rss>
