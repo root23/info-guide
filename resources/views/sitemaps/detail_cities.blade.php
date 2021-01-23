9adf79ba2f84b6f9d6fe0802f02841b8f336a801
root23 <hipstor.pnz@gmail.com>
2021-01-23 18:24:01 +0300

    upd organizations sitemap

--- resources/views/sitemaps/detail_cities.blade.php
+++ resources/views/sitemaps/detail_cities.blade.php
@@ -1,35 +1,35 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
     @foreach($cities as $city)
         <url>
-            <loc>https://info-guide.ru/cities/{{ $city->slug }}/</loc>
+            <loc>https://info-guide.ru/kompanii/{{ $city->slug }}/</loc>
             <lastmod>{{ $city->updated_at->tz('UTC')->toAtomString() }}</lastmod>
         </url>
         <url>
-            <loc>https://info-guide.ru/cities/{{ $city->slug }}/sto/</loc>
+            <loc>https://info-guide.ru/kompanii/{{ $city->slug }}/sto/</loc>
             <lastmod>{{ $city->updated_at->tz('UTC')->toAtomString() }}</lastmod>
         </url>
         <url>
-            <loc>https://info-guide.ru/cities/{{ $city->slug }}/medicina/</loc>
+            <loc>https://info-guide.ru/kompanii/{{ $city->slug }}/medicina/</loc>
             <lastmod>{{ $city->updated_at->tz('UTC')->toAtomString() }}</lastmod>
         </url>
         <url>
-            <loc>https://info-guide.ru/cities/{{ $city->slug }}/restorany/</loc>
+            <loc>https://info-guide.ru/kompanii/{{ $city->slug }}/restorany/</loc>
             <lastmod>{{ $city->updated_at->tz('UTC')->toAtomString() }}</lastmod>
         </url>
         <url>
-            <loc>https://info-guide.ru/cities/{{ $city->slug }}/sport/</loc>
+            <loc>https://info-guide.ru/kompanii/{{ $city->slug }}/sport/</loc>
             <lastmod>{{ $city->updated_at->tz('UTC')->toAtomString() }}</lastmod>
         </url>
         <url>
-            <loc>https://info-guide.ru/cities/{{ $city->slug }}/magaziny/</loc>
+            <loc>https://info-guide.ru/kompanii/{{ $city->slug }}/magaziny/</loc>
             <lastmod>{{ $city->updated_at->tz('UTC')->toAtomString() }}</lastmod>
         </url>
         <url>
-            <loc>https://info-guide.ru/cities/{{ $city->slug }}/kinoteatry/</loc>
+            <loc>https://info-guide.ru/kompanii/{{ $city->slug }}/kinoteatry/</loc>
             <lastmod>{{ $city->updated_at->tz('UTC')->toAtomString() }}</lastmod>
         </url>
         <url>
-            <loc>https://info-guide.ru/cities/{{ $city->slug }}/bary-i-kluby</loc>
+            <loc>https://info-guide.ru/kompanii/{{ $city->slug }}/bary-i-kluby</loc>
             <lastmod>{{ $city->updated_at->tz('UTC')->toAtomString() }}</lastmod>
         </url>
     @endforeach
