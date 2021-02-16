b8bc0e47723928e15d18e3e6769e2d5b772aa3bc
root23 <hipstor.pnz@gmail.com>
2021-02-16 22:26:31 +0300

    ?

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -20,7 +20,7 @@ class BlogPost extends Model
 
     const UNKNOWN_USER = 1;
 
-    protected $fillable = ['category_id', 'slug', 'title', 'excert', 'content_raw', 'content_html', 'is_published', 'published_at','img', 'view_count', 'meta_description', 'meta_keywords'];
+    protected $fillable = ['category_id', 'slug', 'title', 'excert', 'cogitntent_raw', 'content_html', 'is_published', 'published_at','img', 'view_count', 'meta_description', 'meta_keywords'];
 
     /**
      * Категория статьи
