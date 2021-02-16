dcd7a67df794b7bc9087fddb69fa1a0782feb7b7
root23 <hipstor.pnz@gmail.com>
2021-02-16 22:54:44 +0300

    initial commit

---
+++ app/Http/Controllers/Api/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function index() {
+        return 'hello world';
+    }
+}
